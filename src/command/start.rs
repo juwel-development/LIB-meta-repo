a5c2e3160c728c4d8b104b200094a7d3ba5da0c2
Jens-Uwe Loessl <mail@jens-uwe-loessl.de>
2024-06-16 00:25:26 +0200

    feat: run start command in app repository

--- src/command/start.rs
+++ src/command/start.rs
@@ -1,3 +1,30 @@
-pub fn start(app: String) {
-    println!("Starting app: {}", app);
+use crate::configuration::config::Config;
+
+pub fn start(app_name: String) {
+    println!("Starting app: {}", app_name);
+
+    // find the app in the config
+    let config = Config::read_config();
+    let app_config = config.apps.iter().find(|app| {
+        let name = app.get_name_from_package_json();
+
+        return name == app_name;
+    });
+
+    if app_config.is_none() {
+        eprintln!("App not found in config");
+        return;
+    }
+
+    let app_config = app_config.unwrap();
+    run_npm_start(&app_config.dir);
+}
+
+fn run_npm_start(dir: &String) {
+    let _ = std::process::Command::new("npm")
+        .arg("start")
+        .stdin(std::process::Stdio::inherit())
+        .stdout(std::process::Stdio::inherit())
+        .current_dir(dir)
+        .output();
 }
--- src/configuration/app.rs
+++ src/configuration/app.rs
@@ -1,3 +1,7 @@
+use std::fs::File;
+use std::io::BufReader;
+
+use serde_json::Value;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -12,5 +16,14 @@ impl App {
             dir: "".to_string(),
             git: "".to_string(),
         }
+    }
+
+    pub fn get_name_from_package_json(&self) -> String {
+        let package_json = format!("{}/package.json", self.dir);
+        let file = File::open(package_json).unwrap();
+        let reader = BufReader::new(file);
+        let package_json: Value = serde_json::from_reader(reader).unwrap();
+        let name = package_json["name"].as_str().unwrap();
+        return name.to_string();
     }
 }
