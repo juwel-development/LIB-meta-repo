e9899c066a49c8a560f04074b9b877caddeec824
Jens-Uwe Loessl <mail@jens-uwe-loessl.de>
2024-06-26 14:49:27 +0200

    fix: can not resolve binary

--- npm/meta-repo/src/index.ts
+++ npm/meta-repo/src/index.ts
@@ -21,7 +21,7 @@ function getExePath() {
 
   try {
     // Since the binary will be located inside `node_modules`, we can simply call `require.resolve`
-    return require.resolve(`meta-repo-${os}-${arch}/bin/meta-repo${extension}`);
+    return require.resolve(`@juwel-development/meta-repo-${os}-${arch}/bin/meta-repo${extension}`);
   } catch (e) {
     throw new Error(
       `Couldn't find application binary inside node_modules for ${os}-${arch}`
